96739738325e545e618c046d2695777f1a3c0b95
bibilchacko151@gmail.com <bibilchacko151@gmail.com>
2025-12-29 13:06:56 +0530

    first commit

---
+++ src/main/java/com/k15labs/utils/Function.java
@@ -0,0 +1,56 @@
+package com.k15labs.utils;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function {
+    /**
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
+     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     */
+    @FunctionName("callback")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) 
+    {
+    	 Logger logger = context.getLogger();
+    	HttpResponseMessage httpResponseMessage = request.createResponseBuilder(HttpStatus.OK).body("OK").build();
+        context.getLogger().info("SMS Call Back Triggered From Twilio");
+        Map<String, String> queryParameters = request.getQueryParameters();
+        if(queryParameters!=null) {
+        	 queryParameters.forEach((key,value)->{
+             	logger.log(Level.INFO,"Query Parameter {0}:{1}", new Object[] {key,value});
+             });
+        }
+        Optional<String> body = request.getBody();
+        if(body.isPresent()) 
+        {
+        	logger.log(Level.INFO,"Query Parameter {0}", body.get());
+        }
+       
+
+ 
+     
+        return httpResponseMessage;
+       
+    }
+}
